029ab4d51227d5d05b8ca05bb9382e0fcbd8f699
Andrea Parodi <andrea@parro.it>
2023-11-28 11:39:34 +0100

    fixes

--- cli/magda_rd2wrf/cli.go
+++ cli/magda_rd2wrf/cli.go
@@ -5,28 +5,40 @@ import (
 	"io"
 	"log"
 	"os"
+	"time"
 
 	"github.com/meteocima/magda_rd2wrf/radar"
 )
 
 func main() {
 	if len(os.Args) < 3 {
-		log.Fatal("usage: r2w <inputdir> <outfilename> YYYYMMDDHH")
+		log.Fatal("usage: magda_rd2wrf <inputdir> <outfilename> YYYYMMDDHHNN")
 	}
-	reader, err := radar.Convert(os.Args[1], os.Args[2], os.Args[3])
-	if err != nil {
+
+	var instant time.Time
+	var reader io.Reader
+	var outfile *os.File
+
+	var err error
+
+	if instant, err = time.Parse("200601021504", os.Args[3]); err != nil {
 		log.Fatal(err)
 	}
-	outfile, err := os.OpenFile(os.Args[2], os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.FileMode(0644))
-	if err != nil {
+
+	flags := os.O_CREATE | os.O_WRONLY | os.O_TRUNC
+	m := os.FileMode(0644)
+	if outfile, err = os.OpenFile(os.Args[2], flags, m); err != nil {
+		log.Fatal(err)
+	}
+	defer outfile.Close()
+
+	if reader, err = radar.Convert(os.Args[1], instant); err != nil {
 		log.Fatal(err)
 	}
 
 	outfileBuff := bufio.NewWriter(outfile)
 
-	defer outfile.Close()
-	_, err = io.Copy(outfileBuff, reader)
-	if err != nil {
+	if _, err = io.Copy(outfileBuff, reader); err != nil {
 		log.Fatal(err)
 	}
 }
---
+++ radar/convert.go
@@ -0,0 +1,233 @@
+package radar
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/fhs/go-netcdf/netcdf"
+)
+
+func filenameForLev(dirname string, cappilev int, dt time.Time) string {
+	pt := fmt.Sprintf("%s/%s_cappi%dkm.nc", dirname, dt.Format("20060102_1504"), cappilev)
+	return pt
+}
+
+func writeRadarData(f io.Writer, val float32, height float64) {
+
+	if val < 0 {
+		// write(301,'(3x,f12.1,2(f12.3,i4,f12.3,2x))')
+		// hgt(i,m), rv_data(i,m), rv_qc(i,m), rv_err(i,m), rf_data(i,m), rf_qc(i,m), rf_err(i,m)
+		fmt.Fprintf(f, "   %12.1f -888888.000 -88 -888888.000   -888888.000 -88 -888888.000\n", height)
+		return
+	}
+
+	fmt.Fprintf(
+		f,
+		// write(301,'(3x,f12.1,2(f12.3,i4,f12.3,2x))')
+		// hgt(i,m), rv_data(i,m), rv_qc(i,m), rv_err(i,m), rf_data(i,m), rf_qc(i,m), rf_err(i,m)
+		"   %12.1f -888888.000 -88 -888888.000  %12.3f   0       5.000\n",
+		height,
+		val,
+	)
+
+}
+
+func writeConvertedDataTo(resultW io.WriteCloser, dims *MosaicData, dtRequested time.Time) {
+	fmt.Println("writeConvertedDataTo")
+
+	defer resultW.Close()
+	result := bufio.NewWriterSize(resultW, 1000000)
+	defer result.Flush()
+
+	maxLon := float32(-1)
+	maxLat := float32(-1)
+	instant := dtRequested.Format("2006-01-02_15:04")
+	totObs := 0
+
+	if dims.Cappi2 != nil ||
+		dims.Cappi3 != nil ||
+		dims.Cappi4 != nil ||
+		dims.Cappi5 != nil {
+		for _, l := range dims.Lon {
+			if l > maxLon {
+				maxLon = l
+			}
+		}
+		for _, l := range dims.Lat {
+			if l > maxLat {
+				maxLat = l
+			}
+		}
+	} else {
+		maxLon = float32(1)
+		maxLat = float32(1)
+	}
+
+	for i := int64(0); i < dims.Width*dims.Height; i++ {
+		f2 := float32(-1)
+		f3 := float32(-1)
+		f4 := float32(-1)
+		f5 := float32(-1)
+
+		if dims.Cappi2 != nil {
+			f2 = dims.Cappi2[i]
+		}
+
+		if dims.Cappi3 != nil {
+			f3 = dims.Cappi3[i]
+		}
+
+		if dims.Cappi4 != nil {
+			f4 = dims.Cappi4[i]
+		}
+
+		if dims.Cappi5 != nil {
+			f5 = dims.Cappi5[i]
+		}
+
+		if f2 >= 0 || f3 >= 0 || f4 >= 0 || f5 >= 0 {
+			totObs++
+		}
+	}
+
+	fmt.Fprintf(result, "TOTAL NUMBER =  1\n")
+	fmt.Fprintf(result, "#-----------------#\n")
+	fmt.Fprintf(result, "\n")
+	//  write(301,'(a5,2x,a12,2(f8.3,2x),f8.1,2x,a19,2i6)') 'RADAR', &
+	//  radar_name, rlonr(irad), rlatr(irad), raltr(irad)*1000., &
+	//  trim(radar_date), np, imdv_nz(irad)
+	fmt.Fprintf(result, "RADAR              %8.3f  %8.3f     100.0  %s:00%6d     4\n",
+		maxLon,
+		maxLat,
+		instant,
+		totObs,
+	)
+
+	fmt.Fprintf(result, "#-------------------------------------------------------------------------------#\n")
+	fmt.Fprintf(result, "\n")
+
+	if dims.Cappi2 == nil &&
+		dims.Cappi3 == nil &&
+		dims.Cappi4 == nil &&
+		dims.Cappi5 == nil {
+		return
+	}
+
+	instant = dims.Instants[0].Format("2006-01-02_15:04")
+
+	for x := int64(0); x < dims.Width; x++ {
+		for y := int64(dims.Height) - 1; y >= int64(0); y-- {
+			i := x + y*dims.Width
+
+			lat := dims.Lat[i]
+			lon := dims.Lon[i]
+
+			f2 := float32(-1)
+			f3 := float32(-1)
+			f4 := float32(-1)
+			f5 := float32(-1)
+
+			if dims.Cappi2 != nil {
+				f2 = dims.Cappi2[i]
+			}
+			if dims.Cappi3 != nil {
+				f3 = dims.Cappi3[i]
+			}
+			if dims.Cappi4 != nil {
+				f4 = dims.Cappi4[i]
+			}
+			if dims.Cappi5 != nil {
+				f5 = dims.Cappi5[i]
+			}
+
+			if f2 >= 0 || f3 >= 0 || f4 >= 0 || f5 >= 0 {
+				fmt.Fprintf(
+					result,
+					//!----Write data
+					//do i = 1,np ! np: # of total horizontal data points
+					//write(301,'(a12,3x,a19,2x,2(f12.3,2x),f8.1,2x,i6)') 'FM-128 RADAR', &
+					// trim(radar_date), plat(i), plon(i), raltr(irad)*1000, count_nz(i)
+
+					//"FM-128 RADAR   %s:00       %7.3f      %8.3f     100.0       3\n",
+					"FM-128 RADAR   %s:00  %12.3f  %12.3f     100.0       4\n",
+					instant,
+					lat,
+					lon)
+
+				writeRadarData(result, f2, 2000.0)
+				writeRadarData(result, f3, 3000.0)
+				writeRadarData(result, f4, 4000.0)
+				writeRadarData(result, f5, 5000.0)
+			}
+		}
+	}
+}
+
+func readDataFromFile(mos *MosaicData, dirname string, dt time.Time, dest *[]float32, cappilev int) error {
+	var err error
+	var ds netcdf.Dataset
+
+	if ds, err = netcdf.OpenFile(filenameForLev(dirname, cappilev, dt), netcdf.FileMode(netcdf.NOWRITE)); err != nil {
+		return err
+	}
+	if mos.Width == -1 {
+		fmt.Println("MosaicData dimensions not initialized.")
+
+		if mos.Width, err = GetDimensionLen(&ds, "cols"); err != nil {
+			return err
+		}
+		fmt.Println("Width", mos.Width)
+
+		if mos.Height, err = GetDimensionLen(&ds, "rows"); err != nil {
+			return err
+		}
+		fmt.Println("Height", mos.Height)
+
+		if mos.Lat, err = ReadFloatVar(&ds, "lat"); err != nil {
+			return err
+		}
+		fmt.Println("Latitude", mos.Lat)
+
+		if mos.Lon, err = ReadFloatVar(&ds, "lon"); err != nil {
+			return err
+		}
+		fmt.Println("Longitude", mos.Lon)
+
+		if mos.Instants, err = ReadTimeVar(&ds, "time"); err != nil {
+			return err
+		}
+		fmt.Println("Time", mos.Instants)
+	}
+	if *dest, err = ReadFloatVar(&ds, "DBZH"); err != nil {
+		return err
+	}
+
+	return ds.Close()
+}
+
+// Convert ...
+func Convert(dirname string, dt time.Time) (io.Reader, error) {
+	mos := MosaicData{
+		Width: -1,
+	}
+	var err error
+	if err = readDataFromFile(&mos, dirname, dt, &mos.Cappi2, 2); err != nil {
+		return nil, err
+	}
+	if err = readDataFromFile(&mos, dirname, dt, &mos.Cappi3, 3); err != nil {
+		return nil, err
+	}
+	if err = readDataFromFile(&mos, dirname, dt, &mos.Cappi4, 4); err != nil {
+		return nil, err
+	}
+	if err = readDataFromFile(&mos, dirname, dt, &mos.Cappi5, 5); err != nil {
+		return nil, err
+	}
+
+	reader, result := io.Pipe()
+
+	go writeConvertedDataTo(result, &mos, dt)
+	return reader, err
+}
